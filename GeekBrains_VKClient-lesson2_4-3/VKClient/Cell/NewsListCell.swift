643a3d5966e379a44a47d74c44871974204ca88c
Yuri <mandalai88@mail.ru>
2020-12-03 14:30:01 +0300

    home work check2

---
+++ GeekBrains_VKClient-lesson2_4-3/VKClient/Cell/NewsListCell.swift
@@ -0,0 +1,20 @@
+//
+//  NewsListCell.swift
+//  VKClient
+//
+
+
+import UIKit
+
+class NewsListCell: UITableViewCell {
+    @IBOutlet weak var newsDescriptionLabel: UILabel!
+    @IBOutlet weak var newsImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+}
---
+++ GeekBrains_VKClient-lesson2_4-3/VKClient/Controllers/FriendPageViewController.swift
@@ -0,0 +1,40 @@
+//
+//  FriendPageViewController.swift
+//  VKClient
+//
+
+//
+
+import UIKit
+
+class FriendPageViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var selectedFriend: Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.dataSource = self
+        
+        NetworkService.shared.getUserPhotosList(token: Session.shared.token, userID: Session.shared.userId)
+    }
+}
+
+extension FriendPageViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return workingFriendsListCatalogue[selectedFriend].friendPhoto.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FriendPageCell", for: indexPath) as! FriendPageCell
+        
+        cell.userPic.image = workingFriendsListCatalogue[selectedFriend].friendPhoto[indexPath.row].photoName
+        cell.photoCounter = indexPath.row
+        cell.friendIndex = selectedFriend
+        cell.setUpLikeControl()
+        
+        return cell
+    }
+}
