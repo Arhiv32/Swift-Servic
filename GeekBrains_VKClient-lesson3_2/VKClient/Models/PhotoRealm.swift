2926190d39e2ce21885f845c7c0a42bf5ac763ca
Yuri <mandalai88@mail.ru>
2020-12-31 12:51:50 +0300

    home work check4

---
+++ GeekBrains_VKClient-lesson3_2/VKClient/Models/PhotoRealm.swift
@@ -0,0 +1,42 @@
+//
+//  PhotoRealm.swift
+//  VKClient
+//
+
+
+import Foundation
+import RealmSwift
+
+class FriendPhotosCodable: Codable {
+    let response: FriendPhotosResponse
+}
+
+class FriendPhotosResponse: Codable {
+    let count: Int
+    let items: [FriendPhotosItem]
+}
+
+class FriendPhotosItem: Object, Codable {
+    var albumID = RealmOptional<Int>()
+    var date = RealmOptional<Int>()
+    var id = RealmOptional<Int>()
+    var ownerID = RealmOptional<Int>()
+    var hasTags = RealmOptional<Bool>()
+    var sizes = List<FriendPhotosSize>()
+    @objc dynamic var text: String? = nil
+
+    enum CodingKeys: String, CodingKey {
+        case albumID = "album_id"
+        case date, id
+        case ownerID = "owner_id"
+        case hasTags = "has_tags"
+        case sizes, text
+    }
+}
+
+class FriendPhotosSize: Object, Codable {
+    var height = RealmOptional<Int>()
+    @objc dynamic var url: String? = nil
+    @objc dynamic var type: String? = nil
+    var width = RealmOptional<Int>()
+}
---
+++ GeekBrains_VKClient-lesson3_2/VKClient/View/NameLetterCellView.swift
@@ -0,0 +1,11 @@
+//
+//  NameLetterCellView.swift
+//  VKClient
+//
+
+import UIKit
+
+class NameLetterCell: UITableViewCell {
+    @IBOutlet weak var userNameLetter: UILabel!
+    
+}
